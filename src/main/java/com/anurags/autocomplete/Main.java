41b3f6a60b9d339164da2599eb00bd900d33ea88
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2024-05-18 23:05:45 +0530

    fix

--- src/main/java/com/anurags/autocomplete/Main.java
+++ src/main/java/com/anurags/autocomplete/Main.java
@@ -1,17 +1,32 @@
 package com.anurags.autocomplete;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
-  public static void main(String[] args) {
-    AutoComplete ac = new AutoComplete();
-    String fileName = "rockyou.txt";
-    ac.feedFile(fileName);
+    public static void main(String[] args) {
+        AutoComplete ac = new AutoComplete();
+        String fileName = "rockyou.txt";
+        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(fileName);
+        if(inputStream == null) {
+            System.out.println("File not found");
+            return;
+        }
+        // print all the resources
 
-    List<String> result = ac.search("josh");
-    for (String word : result) {
-      System.out.println(word);
+        try(BufferedReader br = new BufferedReader(new java.io.InputStreamReader(inputStream))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                ac.insert(line);
+            }
+            System.out.println("Enter the prefix to search");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-  }
 }
 
